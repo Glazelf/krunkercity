af2fe710876b9f04523366bc872a4310b1acc005
Mathijs de Moor <mathijsdmoor@hotmail.com>
2018-12-13 13:17:18 +0100

    Helpscreen finished

--- build/app.js
+++ build/app.js
@@ -90,18 +90,42 @@ class Helpscreen {
         this._canvas = canvas;
         this._backgroundhelp = new Image();
         this._backgroundhelp.src = "./assets/images/greenenergy.jpg";
-        this._textBubble = new Image();
-        this._textBubble.src = "./assets/images/text_bubblecolored.png";
-        this._zonnenPaneel = new Image();
-        this._zonnenPaneel.src = "./assets/images/isometric_zonnenpaneel.png";
+        this._munten = new Image();
+        this._munten.src = "./assets/images/munten_uitleg.png";
+        this._energiePunten = new Image();
+        this._energiePunten.src = "./assets/images/energie_uitleg.png";
+        this._groeneEnergie = new Image();
+        this._groeneEnergie.src = "./assets/images/groen_uitleg.png";
+        this._co2 = new Image();
+        this._co2.src = "./assets/images/co2_uitleg.png";
         this._windMolen = new Image();
-        this._windMolen.src = "./assets/images/isometric_windmolens.png";
+        this._windMolen.src = "./assets/images/windmolen_uitleg.png";
+        this._zonnePanelen = new Image();
+        this._zonnePanelen.src = "./assets/images/zonnepaneel_uitleg.png";
+        this._stuwDam = new Image();
+        this._stuwDam.src = "./assets/images/stuwdam_uitleg.png";
+        this._aardWarmte = new Image();
+        this._aardWarmte.src = "./assets/images/aardwarmte_uitleg.png";
+        this._kernCentrale = new Image();
+        this._kernCentrale.src = "./assets/images/kerncentrale_uitleg.png";
+        this._kolenCentrale = new Image();
+        this._kolenCentrale.src = "./assets/images/kolencentrale_uitleg.png";
+        this._menuKnop = new Image();
+        this._menuKnop.src = "./assets/images/menu_knop.png";
     }
     drawHelp() {
         this._canvas.writeImageToCanvas(this._backgroundhelp, 0, 0, this._canvas.getWidth(), this._canvas.getHeight());
-        this._canvas.writeImageToCanvas(this._textBubble, this._canvas.getWidth() / 4, this._canvas.getHeight() / 64, this._textBubble.width * 1.2, this._textBubble.height * 1.2);
-        this._canvas.writeImageToCanvas(this._zonnenPaneel, this._canvas.getWidth() / 8, this._canvas.getHeight() / 64, this._textBubble.width, this._textBubble.height);
-        this._canvas.writeImageToCanvas(this._windMolen, this._canvas.getWidth() / 6, this._canvas.getHeight() / 8, this._textBubble.width, this._textBubble.height);
+        this._canvas.writeImageToCanvas(this._munten, this._canvas.getWidth() / 32, this._canvas.getHeight() / 64, this._munten.width / 3.2, this._munten.height / 3.2);
+        this._canvas.writeImageToCanvas(this._energiePunten, this._canvas.getWidth() / 2.87, this._canvas.getHeight() / 64, this._energiePunten.width / 3.2, this._energiePunten.height / 3.2);
+        this._canvas.writeImageToCanvas(this._groeneEnergie, this._canvas.getWidth() / 1.5, this._canvas.getHeight() / 64, this._groeneEnergie.width / 3.2, this._groeneEnergie.height / 3.2);
+        this._canvas.writeImageToCanvas(this._co2, this._canvas.getWidth() / 32, this._canvas.getHeight() / 4, this._co2.width / 3.2, this._co2.height / 3.2);
+        this._canvas.writeImageToCanvas(this._windMolen, this._canvas.getWidth() / 2.87, this._canvas.getHeight() / 4, this._windMolen.width / 3.2, this._windMolen.height / 3.2);
+        this._canvas.writeImageToCanvas(this._zonnePanelen, this._canvas.getWidth() / 1.5, this._canvas.getHeight() / 4, this._zonnePanelen.width / 3.2, this._zonnePanelen.height / 3.2);
+        this._canvas.writeImageToCanvas(this._stuwDam, this._canvas.getWidth() / 32, this._canvas.getHeight() / 2.07, this._stuwDam.width / 3.2, this._stuwDam.height / 3.2);
+        this._canvas.writeImageToCanvas(this._aardWarmte, this._canvas.getWidth() / 2.87, this._canvas.getHeight() / 2.07, this._aardWarmte.width / 3.2, this._aardWarmte.height / 3.2);
+        this._canvas.writeImageToCanvas(this._kernCentrale, this._canvas.getWidth() / 1.5, this._canvas.getHeight() / 2.07, this._kernCentrale.width / 3.2, this._kernCentrale.height / 3.2);
+        this._canvas.writeImageToCanvas(this._kolenCentrale, this._canvas.getWidth() / 2.87, this._canvas.getHeight() / 1.4, this._kolenCentrale.width / 3.2, this._kolenCentrale.height / 3.2);
+        this._canvas.writeImageToCanvas(this._menuKnop, this._canvas.getWidth() / 7.5, this._canvas.getHeight() / 1.3, this._menuKnop.width / 3.2, this._menuKnop.height / 3.2);
     }
     ;
 }
