9d866b007b99bdc8089b4cbdaba15e62ca386dde
Glazelf <gideon.steenbergen@gmail.com>
2018-12-13 11:59:42 +0100

    typo

--- src/Startscreen.ts
+++ src/Startscreen.ts
@@ -3,6 +3,7 @@ class Startscreen {
     private readonly _ctx: CanvasRenderingContext2D;
     private _game: Game;
     private _canvas: Canvas;
+    private _levelHelper: LevelHelper;
     private levelsUnlocked: number = 1;
     private userName: string;
     public _backgroundstart: HTMLImageElement;
@@ -20,7 +21,7 @@ class Startscreen {
         this._level2background.src = "./assets/images/level2bubble.png";
         this._level3background = new Image();
         this._level3background.src = "./assets/images/level3bubble.png";
-        console.log(canvas);
+        //console.log(canvas);
     }
 
     public draw = () => {
@@ -28,6 +29,6 @@ class Startscreen {
         this._canvas.writeImageToCanvas(this._backgroundstart, 0, 0, this._canvas.getWidth(), this._canvas.getHeight());
         this._canvas.writeImageToCanvas(this._level1background, this._canvas.getWidth() / 40, this._canvas.getHeight() / 3.5, this._level1background.width / 2, this._level1background.height / 2);
         this._canvas.writeImageToCanvas(this._level2background, this._canvas.getWidth() / 2.9, this._canvas.getHeight() / 3.5, this._level2background.width / 2, this._level2background.height / 2);
-        this._canvas.writeImageToCanvas(this._level3background, this._canvas.getWidth() / 1.5, this._canvas.getHeight() / 3.5, this._level2background.width / 2, this._level2background.height / 2);
+        this._canvas.writeImageToCanvas(this._level3background, this._canvas.getWidth() / 1.5, this._canvas.getHeight() / 3.5, this._level3background.width / 2, this._level3background.height / 2);
     };
 };
