483fb9f092cb8de1f8da3c5aae2132b0bcac9296
Glazelf <gideon.steenbergen@gmail.com>
2018-12-13 11:58:59 +0100

    hammermethod

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,12 +1,16 @@
 class Canvas {
     private readonly _canvasElement: HTMLCanvasElement;
     private readonly _ctx: CanvasRenderingContext2D;
+    private _levelHelper: LevelHelper;
+    public _buildingHammer: HTMLImageElement;
 
     constructor(canvasID: HTMLCanvasElement) {
         this._canvasElement = canvasID;
         this._ctx = this._canvasElement.getContext('2d');
         this._canvasElement.width = window.innerWidth;
         this._canvasElement.height = window.innerHeight;
+        this._buildingHammer = new Image();
+        this._buildingHammer.src = "./assets/images/hammer.png";
     };
 
     public clearScreen(): void {
@@ -37,6 +41,17 @@ class Canvas {
     ) {
         this._ctx.drawImage(element, xCoordinate, yCoordinate, width, height);
     };
+
+    public writeHammerToCanvas( 
+        element: HTMLImageElement = this._buildingHammer,
+        xCoordinate: number,
+        yCoordinate: number,
+        width: number = this._buildingHammer.width/7,
+        height: number = this._buildingHammer.height/7,
+    ) {
+        this._ctx.drawImage(element, xCoordinate, yCoordinate, width, height);
+    };
+
     public randomNumber(min: number, max: number): number {
         return Math.round(Math.random() * (max - min) + min);
     };
