41bc33f3f94dcaeefd499ea33d13e841c2f22449
Glazelf <gideon.steenbergen@gmail.com>
2018-12-13 11:59:25 +0100

    drawlevel

--- src/Game.ts
+++ src/Game.ts
@@ -4,27 +4,32 @@ class Game {
     private _canvas: Canvas;
     private _startscreen: Startscreen;
     private _Helpscreen: Helpscreen;
+    private _levelHelper: LevelHelper;
 
     constructor() {
         this._canvasElement = <HTMLCanvasElement>document.getElementById('canvas');
         this._canvas = new Canvas(this._canvasElement);
         this._startscreen = new Startscreen(this._canvas);
         this._Helpscreen = new Helpscreen(this._canvas);
+        this._levelHelper = new LevelHelper(this._canvas)
     };
 
-    public drawStart=()=>{
-        console.log(this._canvas)
-        this._startscreen.draw();
-    };
+    // public drawStart=()=>{
+    //     console.log(this._canvas)
+    //     this._startscreen.draw();
+    // };
 
     // public drawHelp=()=>{
-    //     //console.log(this._canvas)
     //     this._Helpscreen.drawHelp();
     // };
+
+    public drawLevel=()=>{
+        this._levelHelper.drawLevel1()
+    }
 };
 
 window.addEventListener('load', init);
 function init(): void {
     const KrunkerCity = new Game();
-    window.setInterval(KrunkerCity.drawStart, 1000 / 60);
+    window.setInterval(KrunkerCity.drawLevel, 1000 / 60);
 };
