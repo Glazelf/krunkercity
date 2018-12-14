806768c0ed23bd2324415995e1724abbb03cd87d
Glazelf <gideon.steenbergen@gmail.com>
2018-12-14 10:14:27 +0100

    drawCurrencies

--- build/app.js
+++ build/app.js
@@ -68,7 +68,7 @@ class Game {
 window.addEventListener('load', init);
 function init() {
     const KrunkerCity = new Game();
-    window.setInterval(KrunkerCity.drawStart, 1000 / 60);
+    window.setInterval(KrunkerCity.drawLevel, 1000 / 60);
 }
 ;
 class GameController {
@@ -188,6 +188,9 @@ class Helpscreen {
 ;
 class LevelHelper {
     constructor(canvas) {
+        this.co2 = 50;
+        this.energy = 0;
+        this.money = 100;
         this._canvas = canvas;
         this._level1background = new Image();
         this._level1background.src = "./assets/images/level1.png";
@@ -195,25 +198,37 @@ class LevelHelper {
         this._level2background.src = "./assets/images/level2.png";
         this._level3background = new Image();
         this._level3background.src = "./assets/images/level3.png";
+        this._bolt = new Image();
+        this._bolt.src = "./assets/images/bolt.png";
+        this._pollution = new Image();
+        this._pollution.src = "";
+        this._coins = new Image();
+        this._coins.src = "./assets/images/simmoney.png";
         console.log(canvas);
     }
     drawLevel1() {
         this._canvas.writeImageToCanvas(this._level1background, 0, 0, this._canvas.getWidth(), this._canvas.getHeight());
         this._canvas.writeHammerToCanvas(this._canvas._buildingHammer, this._canvas.getWidth() / 10, this._canvas.getHeight() / 9);
         this._canvas.writeHammerToCanvas(this._canvas._buildingHammer, this._canvas.getWidth() / 10, this._canvas.getHeight() / 9);
+        this.drawCurrencies();
     }
     ;
     drawLevel2() {
         this._canvas.writeImageToCanvas(this._level2background, 0, 0, this._canvas.getWidth(), this._canvas.getHeight());
+        this.drawCurrencies();
     }
     ;
     drawLevel3() {
         this._canvas.writeImageToCanvas(this._level3background, 0, 0, this._canvas.getWidth(), this._canvas.getHeight());
+        this.drawCurrencies();
     }
     ;
-    drawBuildSpots() {
+    drawCurrencies() {
+        this._canvas.writeImageToCanvas(this._bolt, this._canvas.getWidth() / 500, this._canvas.getHeight() / 10, this._bolt.width / 8, this._bolt.height / 8);
+        this._canvas.writeTextToCanvas(`${this.energy}`, this._canvas.getWidth() / 14.5, this._canvas.getHeight() / 6.1, 60);
+        this._canvas.writeImageToCanvas(this._coins, this._canvas.getWidth() / 500, this._canvas.getHeight() / 5.5, this._coins.width / 3, this._coins.height / 3);
+        this._canvas.writeTextToCanvas(`${this.money}`, this._canvas.getWidth() / 14.5, this._canvas.getHeight() / 4, 60);
     }
-    ;
 }
 ;
 class Startscreen {
